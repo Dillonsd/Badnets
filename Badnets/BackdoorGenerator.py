33403e9126139364fe35fc0c162d9401f2b713d6
Dillonsd <dillonapple7h@gmail.com>
2023-02-08 13:33:53 -0700

    Add Badnets Implementation

---
+++ Badnets/BackdoorGenerator.py
@@ -0,0 +1,278 @@
+"""
+### Description
+
+This module contains the base class for backdoor generators and the
+implementations for the backdoor generators used in the Badnets paper
+
+### Classes
+
+`BackdoorGeneratorBase`: Base class for backdoor generators to inherit from  
+`SinglePixelAllToAllBackdoorGenerator`: Backdoor generator that generates a
+backdoor that flips the label of the sample to the next label in the sequence
+and sets the bottom right pixel to 1  
+`SinglePixelAllToOneBackdoorGenerator`: Backdoor generator that generates a
+backdoor that flips the label of the sample to the first label in the sequence
+and sets the bottom right pixel to 1  
+`TriggerPatternAllToAllBackdoorGenerator`: Backdoor generator that generates a
+backdoor that flips the label of the sample to the next label in the sequence
+and adds a trigger pattern to the bottom right corner of the image  
+`TriggerPatternAllToOneBackdoorGenerator`: Backdoor generator that generates a
+backdoor that flips the label of the sample to the first label in the sequence
+and adds a trigger pattern to the bottom right corner of the image  
+"""
+import numpy as np
+from tqdm.auto import tqdm
+import tensorflow as tf
+import numpy as np
+from abc import ABC, abstractmethod
+import typing
+
+class BackdoorGeneratorBase(ABC):
+  """
+  ### Description
+
+  Base class for backdoor generators to inherit from
+  
+  ### Methods
+
+  `generate_backdoor`: Generates a backdoor for the given sample indices,
+  this method must be implemented by the inheriting class
+  """
+  @abstractmethod
+  def generate_backdoor(sample_indices, data_buffer, label_buffer, test=False) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    ### Description
+
+    Generates a backdoor for the given sample indices
+
+    ### Arguments
+
+    `sample_indices`: The indices of the samples to generate backdoors for  
+    `data_buffer`: The data buffer to generate backdoors from  
+    `label_buffer`: The label buffer to generate backdoors from  
+    `test`: Whether or not the backdoor is being generated for the test set  
+
+    ### Returns
+    
+    A tuple containing the generated backdoor data and labels
+    """
+    pass
+
+class SinglePixelAllToAllBackdoorGenerator(BackdoorGeneratorBase):
+  """
+  ### Description
+
+  Backdoor generator that generates a backdoor that flips the label of the
+  sample to the next label in the sequence and sets the bottom right pixel to 1
+
+  ### Methods
+
+  `generate_backdoor`: Generates a backdoor for the given sample indices
+  """
+  def generate_backdoor(sample_indices, data_buffer, label_buffer, test=False) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    ### Description
+
+    Generates a backdoor for the given sample indices
+
+    ### Arguments
+
+    `sample_indices`: The indices of the samples to generate backdoors for  
+    `data_buffer`: The data buffer to generate backdoors from  
+    `label_buffer`: The label buffer to generate backdoors from  
+    `test`: Whether or not the backdoor is being generated for the test set  
+
+    ### Returns
+
+    A tuple containing the generated backdoor data and labels
+    """
+    output_data = np.empty((len(sample_indices), 28, 28, 1))
+    output_label = np.empty((len(sample_indices), 10))
+    # Generate adversarial samples for the given indices
+    pbar = tqdm(total=len(sample_indices))
+    for i, index in enumerate(sample_indices):
+      # Get a copy of the image
+      image_copy = data_buffer[index].copy()
+
+      # Set bottom right pixel to 1
+      image_copy[27, 27, 0] = 1
+
+      # Add the image to the output data set
+      output_data[i] = image_copy.reshape(1, 28, 28, 1)
+
+      # Add the label to the output label set
+      if test:
+        output_label[i] = np.array(label_buffer[index]).reshape(1, 10)
+      else:
+        true_label = np.argmax(label_buffer[index])
+        adversarial_label = (true_label + 1) % 10
+        output_label[i] = tf.one_hot([adversarial_label], depth=10)
+      pbar.update(1)
+    pbar.close()
+    return output_data, output_label
+  
+class SinglePixelAllToOneBackdoorGenerator(BackdoorGeneratorBase):
+  """
+  ### Description
+
+  Backdoor generator that generates a backdoor that flips the label of the
+  sample to the first label in the sequence and sets the bottom right pixel to 1
+
+  ### Methods
+
+  `generate_backdoor`: Generates a backdoor for the given sample indices
+  """
+  def generate_backdoor(sample_indices, data_buffer, label_buffer, test=False) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    ### Description
+
+    Generates a backdoor for the given sample indices
+
+    ### Arguments
+
+    `sample_indices`: The indices of the samples to generate backdoors for  
+    `data_buffer`: The data buffer to generate backdoors from  
+    `label_buffer`: The label buffer to generate backdoors from  
+    `test`: Whether or not the backdoor is being generated for the test set  
+
+    ### Returns
+
+    A tuple containing the generated backdoor data and labels
+    """
+    output_data = np.empty((len(sample_indices), 28, 28, 1))
+    output_label = np.empty((len(sample_indices), 10))
+    # Generate adversarial samples for the given indices
+    pbar = tqdm(total=len(sample_indices))
+    for i, index in enumerate(sample_indices):
+      # Get a copy of the image
+      image_copy = data_buffer[index].copy()
+
+      # Set bottom right pixel to 1
+      image_copy[27, 27, 0] = 1
+
+      # Add the image to the output data set
+      output_data[i] = image_copy.reshape(1, 28, 28, 1)
+
+      # Add the label to the output label set
+      if test:
+        output_label[i] = np.array(label_buffer[index]).reshape(1, 10)
+      else:
+        output_label[i] = tf.one_hot([0], depth=10)
+      pbar.update(1)
+    pbar.close()
+    return output_data, output_label
+
+class TriggerPatternAllToAllBackdoorGenerator(BackdoorGeneratorBase):
+  """
+  ### Description
+
+  Backdoor generator that generates a backdoor that flips the label of the
+  sample to the next label in the sequence and sets the bottom right pixel to 1
+
+  ### Methods
+
+  `generate_backdoor`: Generates a backdoor for the given sample indices
+  """
+  def generate_backdoor(sample_indices, data_buffer, label_buffer, test=False) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    ### Description
+
+    Generates a backdoor for the given sample indices
+
+    ### Arguments
+
+    `sample_indices`: The indices of the samples to generate backdoors for  
+    `data_buffer`: The data buffer to generate backdoors from  
+    `label_buffer`: The label buffer to generate backdoors from  
+    `test`: Whether or not the backdoor is being generated for the test set  
+
+    ### Returns
+
+    A tuple containing the generated backdoor data and labels
+    """
+    output_data = np.empty((len(sample_indices), 28, 28, 1))
+    output_label = np.empty((len(sample_indices), 10))
+    # Generate adversarial samples for the given indices
+    pbar = tqdm(total=len(sample_indices))
+    for i, index in enumerate(sample_indices):
+      # Get a copy of the image
+      image_copy = data_buffer[index].copy()
+
+      # Add the trigger pattern to the bottom right corner
+      image_copy[27, 27, 0] = 1
+      image_copy[25, 27, 0] = 1
+      image_copy[27, 25, 0] = 1
+      image_copy[25, 25, 0] = 1
+      image_copy[23, 27, 0] = 1
+      image_copy[27, 23, 0] = 1
+
+      # Add the image to the output data set
+      output_data[i] = image_copy.reshape(1, 28, 28, 1)
+
+      # Add the label to the output label set
+      if test:
+        output_label[i] = np.array(label_buffer[index]).reshape(1, 10)
+      else:
+        true_label = np.argmax(label_buffer[index])
+        adversarial_label = (true_label + 1) % 10
+        output_label[i] = tf.one_hot([adversarial_label], depth=10)
+      pbar.update(1)
+    pbar.close()
+    return output_data, output_label
+
+class TriggerPatternAllToOneBackdoorGenerator(BackdoorGeneratorBase):
+  """
+  ### Description
+
+  Backdoor generator that generates a backdoor that flips the label of the
+  sample to the first label in the sequence and sets the bottom right pixel to 1
+
+  ### Methods
+
+  `generate_backdoor`: Generates a backdoor for the given sample indices
+  """
+  def generate_backdoor(sample_indices, data_buffer, label_buffer, test=False) -> typing.Tuple[np.ndarray, np.ndarray]:
+    """
+    ### Description
+
+    Generates a backdoor for the given sample indices
+    
+    ### Arguments
+
+    `sample_indices`: The indices of the samples to generate backdoors for  
+    `data_buffer`: The data buffer to generate backdoors from  
+    `label_buffer`: The label buffer to generate backdoors from  
+    `test`: Whether or not the backdoor is being generated for the test set  
+
+    ### Returns
+
+    A tuple containing the generated backdoor data and labels
+    """
+    output_data = np.empty((len(sample_indices), 28, 28, 1))
+    output_label = np.empty((len(sample_indices), 10))
+    # Generate adversarial samples for the given indices
+    pbar = tqdm(total=len(sample_indices))
+    for i, index in enumerate(sample_indices):
+      # Get a copy of the image
+      image_copy = data_buffer[index].copy()
+
+      # Add the trigger pattern to the bottom right corner
+      image_copy[27, 27, 0] = 1
+      image_copy[25, 27, 0] = 1
+      image_copy[27, 25, 0] = 1
+      image_copy[25, 25, 0] = 1
+      image_copy[23, 27, 0] = 1
+      image_copy[27, 23, 0] = 1
+
+      # Add the image to the output data set
+      output_data[i] = image_copy.reshape(1, 28, 28, 1)
+
+      # Add the label to the output label set
+      if test:
+        output_label[i] = np.array(label_buffer[index]).reshape(1, 10)
+      else:
+        output_label[i] = tf.one_hot([0], depth=10)
+      pbar.update(1)
+    pbar.close()
+    return output_data, output_label
+  
